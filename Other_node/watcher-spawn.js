845d3555ae9922ab15bee5fbf03ab0488c4c53f6
system1c <gyasis@gmail.com>
2021-03-31 10:28:44 +0000

    file watcher tutorial

--- Other_node/watcher-spawn.js
+++ Other_node/watcher-spawn.js
@@ -2,17 +2,17 @@
 
 const
     fs = require('fs')
-    spawn = require('child_process').spawn,
+spawn = require('child_process').spawn,
     filename = process.argv[2];
 
 if (!filename) {
     throw Errow('A file must be include');
 }
 
-fs.watch(filename, function(){
+fs.watch(filename, function () {
     ls = spawn('ls', ['-lh', filename]);
     ls.stdout.pipe(process.stdout);
 });
 
-console.log('Now watching:',filename );
+console.log('Now watching:', filename);
 
